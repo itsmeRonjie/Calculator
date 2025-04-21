692121606544eb41515c279eeee451678e376e34
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-04-21 14:29:25 +0800

    - added ButtonView

---
+++ Calculator/Views/Sub Views/ButtonView.swift
@@ -0,0 +1,67 @@
+//
+//  ButtonView.swift
+//  Calculator
+//
+//  Created by Ronjie Diafante Man-on on 4/21/25.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    let calcButton: CalcButton
+    let fgColor: Color
+    let bgColor: Color
+    
+    var systemImage: String? {
+        let value = calcButton.rawValue
+        return if value.contains("IMG") {
+            value.replacingOccurrences(
+                of: "IMG",
+                with: ""
+            )
+        } else {
+            nil
+        }
+    }
+    
+    var text: String? {
+        let value = calcButton.rawValue
+        return value.contains("IMG") ? nil : value
+    }
+    
+    let buttonDim: CGFloat = UIScreen.main.bounds.width / 5
+    
+    var body: some View {
+        ZStack {
+            Text(text ?? "")
+            Image(systemName: systemImage ?? "")
+        }
+        .font(.title2)
+        .fontWeight(.semibold)
+        .frame(width: buttonDim, height: buttonDim)
+        .foregroundStyle(fgColor)
+        .background(bgColor)
+        .clipShape(RoundedRectangle(cornerRadius: 15))
+        .shadow(color: bgColor.opacity(0.5), radius: 3, x: 5, y: 5)
+    }
+}
+
+#Preview {
+    VStack {
+        ButtonView(
+            calcButton: .one,
+            fgColor: foregroundDigitsColor,
+            bgColor: buttonBackgroundColor
+        )
+        ButtonView(
+            calcButton: .clear,
+            fgColor: foregroundTopButtonsColor,
+            bgColor: buttonBackgroundColor
+        )
+        ButtonView(
+            calcButton: .negative,
+            fgColor: foregroundRightButtonsColor,
+            bgColor: buttonBackgroundColor
+        )
+    }
+}
