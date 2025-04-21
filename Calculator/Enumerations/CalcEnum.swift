0b514474c4692ff2769646d9e007824d664a9976
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-04-21 14:07:27 +0800

    - added enums

---
+++ Calculator/Enumerations/CalcEnum.swift
@@ -0,0 +1,17 @@
+//
+//  Operation.swift
+//  Calculator
+//
+//  Created by Ronjie Diafante Man-on on 4/21/25.
+//
+
+import Foundation
+
+enum Operation {
+    case add
+    case subtract
+    case multiply
+    case divide
+    case equal
+    case none
+}
