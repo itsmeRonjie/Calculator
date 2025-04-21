c04d9bc4acfed55ae109949ed22190d1eb3e084d
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-04-21 11:43:20 +0800

    - added Colors in Assets - added Constant Colors

---
+++ Calculator/Assets/ImageConstants.swift
@@ -0,0 +1,16 @@
+//
+//  ImageConstants.swift
+//  Calculator
+//
+//  Created by Ronjie Diafante Man-on on 4/21/25.
+//
+
+import Foundation
+import SwiftUI
+
+let buttonBackgroundColor = Color("buttonBackground")
+let foregroundDigitsColor = Color("foregroundDigits")
+let foregroundRightButtonsColor = Color("foregroundRightButtons")
+let foregroundTopButtonsColor = Color("foregroundTopButtons")
+let primaryBackgroundColor = Color("primaryBackground")
+let secondaryBackgroundColor = Color("secondaryBackground")
