550268c5d217d4d2489774bd70ce0985cdfaaa9b
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-04-21 15:00:35 +0800

    - added String Helper

---
+++ Calculator/Utils/StringHelper.swift
@@ -0,0 +1,26 @@
+//
+//  StringHelper.swift
+//  Calculator
+//
+//  Created by Ronjie Diafante Man-on on 4/21/25.
+//
+
+import Foundation
+
+func getLastChar(_ input: String) -> String {
+    guard !input.isEmpty else {
+        return ""
+    }
+    return String(input.last!)
+}
+
+func lastCharIsEqualTo(_ input: String, _ char: String) -> Bool {
+    getLastChar(input) == char
+}
+
+func formatResult(_ result: Double) -> String {
+    let numberFormatter = NumberFormatter()
+    numberFormatter.numberStyle = .decimal
+    numberFormatter.maximumFractionDigits = 16
+    return numberFormatter.string(from: NSNumber(value: result)) ?? "0"
+}
