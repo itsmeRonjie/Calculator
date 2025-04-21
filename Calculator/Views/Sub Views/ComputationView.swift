090216c1e7b279d9146c685c3dcaffcb24357f67
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-04-21 13:28:42 +0800

    - implemented SunMoonView

---
+++ Calculator/Views/Sub Views/ComputationView.swift
@@ -0,0 +1,18 @@
+//
+//  ComputationView.swift
+//  Calculator
+//
+//  Created by Ronjie Diafante Man-on on 4/21/25.
+//
+
+import SwiftUI
+
+struct ComputationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ComputationView()
+}
