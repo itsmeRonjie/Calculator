dd59b23bbf78b0cff9c0e8345e8cbf7126c492df
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-04-21 11:27:18 +0800

    Init

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Ronjie Diafante Man-on on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
