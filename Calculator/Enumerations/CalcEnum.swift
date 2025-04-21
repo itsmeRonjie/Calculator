46921195c87893102424818c361546fecba15c9f
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-04-21 14:07:52 +0800

    - added enums

--- Calculator/Enumerations/CalcEnum.swift
+++ Calculator/Enumerations/CalcEnum.swift
@@ -15,3 +15,34 @@ enum Operation {
     case equal
     case none
 }
+
+
+enum CalcButton: String {
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case zero = "0"
+    case decimal = "."
+    case percent = "%"
+    case clear = "AC"
+    case add = "+"
+    case subtract = "-"
+    case multiply = "×"
+    case divide = "÷"
+    case equals = "="
+    case negative = "IMGplus.forwardslash.minus"
+    case undo = "IMGarrow.counterclockwise"
+}
+
+let enumOperators: [CalcButton] = [.add, .subtract, .multiply, .divide]
+
+let multiplySymbol: String = CalcButton.multiply.rawValue
+let divideSymbol: String = CalcButton.divide.rawValue
+
+let operatros: String = "+-\(multiplySymbol)\(divideSymbol)"
